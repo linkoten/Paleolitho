c4850a20576c215c7f44df5e6a9248c939932acb
linkoten <catto.francois@hotmail.fr>
2024-08-27 19:28:06 +0200

    autre essai

--- lib/authOptions.ts
+++ lib/authOptions.ts
@@ -41,7 +41,7 @@ export const authOptions: NextAuthOptions = {
             pass: process.env.EMAIL_SERVER_PASSWORD
           }
         },
-        from: process.env.EMAIL_FROM
+        from: process.env.EMAIL_FROM,
       }),
     
     ],

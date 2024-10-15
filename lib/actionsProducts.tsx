83158b1b5cbf246f1233fc81e8ae8ca3dd9590bd
linkoten <catto.francois@hotmail.fr>
2024-10-15 15:51:31 +0200

    re

--- lib/actionsProducts.tsx
+++ lib/actionsProducts.tsx
@@ -335,6 +335,7 @@ export const updateProduct = async (formData: FormData) => {
           description,
           price,
           stock,
+          category,
           images,
           country,
           locality,
